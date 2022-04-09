8434803dcfc85806f4b8241105abd63a8502f71a
shanibristman <shanibristman@gmail.com>
2022-04-09 22:43:14 +0300

    test

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,8 +1,12 @@
+
 // this uses the callback syntax, however, we encourage you to try the async/await syntax shown in async-dadjoke.js
+import index from './index'
 export function handler(event, context, callback) {
   console.log('queryStringParameters', event.queryStringParameters)
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({ msg: 'Hello, World!shani' }),
+    // body: JSON.stringify({ msg: 'Hello, World!shani' }),
+    body : index
   })
+
 }
